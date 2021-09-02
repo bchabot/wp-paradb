bf2c460a9429cdd498a29422dbc645fe9ef31621
bchabot <bchabot@gmail.com>
2021-09-02 16:15:48 -0400

    init

--- wp-paradb/admin/class-wpparadb-admin.php
+++ wp-paradb/admin/class-wpparadb-admin.php
@@ -3,11 +3,11 @@
 /**
  * The admin-specific functionality of the plugin.
  *
- * @link       http://example.com
- * @since      1.0.0
- *
- * @package    Plugin_Name
- * @subpackage Plugin_Name/admin
+ * @link              https://github.com/bchabot/wp-paradb
+ * @since             0.0.0
+ * @package           wp-paradb
+ * @subpackage        wp-paradb/admin
+ * @author     Brian Chabot <bchabot@gmail.com>
  */
 
 /**
@@ -16,16 +16,18 @@
  * Defines the plugin name, version, and two examples hooks for how to
  * enqueue the admin-specific stylesheet and JavaScript.
  *
- * @package    Plugin_Name
- * @subpackage Plugin_Name/admin
- * @author     Your Name <email@example.com>
+ * @link              https://github.com/bchabot/wp-paradb
+ * @since             0.0.0
+ * @package           wp-paradb
+ * @subpackage        wp-paradb/admin
+ * @author     Brian Chabot <bchabot@gmail.com>
  */
-class Plugin_Name_Admin {
+class wpparadb_Admin {
 
 	/**
 	 * The ID of this plugin.
 	 *
-	 * @since    1.0.0
+	 * @since    0.0.0
 	 * @access   private
 	 * @var      string    $plugin_name    The ID of this plugin.
 	 */
@@ -34,7 +36,7 @@ class Plugin_Name_Admin {
 	/**
 	 * The version of this plugin.
 	 *
-	 * @since    1.0.0
+	 * @since    0.0.0
 	 * @access   private
 	 * @var      string    $version    The current version of this plugin.
 	 */
@@ -43,7 +45,7 @@ class Plugin_Name_Admin {
 	/**
 	 * Initialize the class and set its properties.
 	 *
-	 * @since    1.0.0
+	 * @since    0.0.0
 	 * @param      string    $plugin_name       The name of this plugin.
 	 * @param      string    $version    The version of this plugin.
 	 */
@@ -57,7 +59,7 @@ class Plugin_Name_Admin {
 	/**
 	 * Register the stylesheets for the admin area.
 	 *
-	 * @since    1.0.0
+	 * @since    0.0.0
 	 */
 	public function enqueue_styles() {
 
@@ -73,14 +75,14 @@ class Plugin_Name_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/plugin-name-admin.css', array(), $this->version, 'all' );
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wpparadb-admin.css', array(), $this->version, 'all' );
 
 	}
 
 	/**
 	 * Register the JavaScript for the admin area.
 	 *
-	 * @since    1.0.0
+	 * @since    0.0.0
 	 */
 	public function enqueue_scripts() {
 
@@ -96,7 +98,7 @@ class Plugin_Name_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/plugin-name-admin.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wpparadb-admin.js', array( 'jquery' ), $this->version, false );
 
 	}
 
