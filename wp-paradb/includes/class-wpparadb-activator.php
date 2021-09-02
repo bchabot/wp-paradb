28580a6f95d8b08f929dad1df085455bcd64d381
bchabot <bchabot@gmail.com>
2021-09-02 15:59:50 -0400

    init

---
+++ wp-paradb/includes/class-wpparadb-activator.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link              https://github.com/bchabot/wp-paradb
+ * @since             0.0.0
+ * @package           wp-paradb
+ * @subpackage        wp-paradb/includes
+ * @author     Brian Chabot <bchabot@gmail.com>
+ */
+class wpparadb_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    0.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
