376efd7e92e02035f0c6f4f60c914e9c4975b807
bchabot <bchabot@gmail.com>
2021-09-02 16:01:21 -0400

    init

---
+++ wp-paradb/includes/class-wpparadbe-deactivator.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link              https://github.com/bchabot/wp-paradb
+ * @since             0.0.0
+ * @package           wp-paradb
+ * @subpackage        wp-paradb/includes
+ * @author     Brian Chabot <bchabot@gmail.com>
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @link              https://github.com/bchabot/wp-paradb
+ * @since             0.0.0
+ * @package           wp-paradb
+ * @subpackage        wp-paradb/includes
+ * @author     Brian Chabot <bchabot@gmail.com>
+ */
+class wpparadb_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    0.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
